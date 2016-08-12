a73123c562179b15be24bfdcdd814f7f7bce5f77
Login Victor <batazor111@gmail.com>
2016-08-13 00:39:57 +0300

    Delete react-dnd

--- src/Components/Content/Content.js
+++ src/Components/Content/Content.js
@@ -1,77 +1,28 @@
-import React, { Component, PropTypes } from 'react';
-import update from 'react/lib/update';
+import React, { Component } from 'react';
+
 import Circle from '../Circle';
-import { DropTarget, DragDropContext } from 'react-dnd';
-import HTML5Backend from 'react-dnd-html5-backend';
 
-const styles = {
-  width: 300,
-  height: 300,
-  border: '1px solid black',
-  position: 'relative'
-};
-
-const boxTarget = {
-  drop(props, monitor, component) {
-    const item = monitor.getItem();
-    const delta = monitor.getDifferenceFromInitialOffset();
-    const left = Math.round(item.left + delta.x);
-    const top = Math.round(item.top + delta.y);
-
-    component.moveBox(item.id, left, top);
-  }
-};
-
-@DragDropContext(HTML5Backend)
-@DropTarget('BOX', boxTarget, connect => ({
-  connectDropTarget: connect.dropTarget()
-}))
-export default class Container extends Component {
-  static propTypes = {
-    connectDropTarget: PropTypes.func.isRequired
-  };
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      boxes: {
-        'a': { top: 20, left: 80, title: 'Drag me around' },
-        'b': { top: 180, left: 20, title: 'Drag me too' }
-      }
-    };
-  }
-
-  moveBox(id, left, top) {
-    this.setState(update(this.state, {
-      boxes: {
-        [id]: {
-          $merge: {
-            left: left,
-            top: top
-          }
-        }
-      }
-    }));
-  }
-
+class Content extends Component {
   render() {
-    const { connectDropTarget } = this.props;
-    const { boxes} = this.state;
+    return (
+      <div>
+        ASIDE
 
-    return connectDropTarget(
-      <div style={styles}>
-        {Object.keys(boxes).map(key => {
-          const { left, top, title } = boxes[key];
-          return (
-            <Circle key={key}
-                 id={key}
-                 left={left}
-                 top={top}>
-              {title}
-            </Circle>
-          );
-        })}
+        <svg
+          width="100%"
+          viewBox="0 0 1000 1000"
+          version="1.1"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <Circle
+            cx={10}
+            cy={10}
+            r={5}
+          />
+        </svg>
       </div>
     );
   }
 }
+
+export default Content;
--- src/app.js
+++ src/app.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import { render } from 'react-dom';
-// import injectTapEventPlugin from 'react-tap-event-plugin';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 
-// import 'normalize.css/normalize.css';
-// import 'flexboxgrid/css/flexboxgrid.min.css';
-// import 'react-virtualized/styles.css';
-// import './app.less';
+import 'normalize.css/normalize.css';
+import 'flexboxgrid/css/flexboxgrid.min.css';
+import 'react-virtualized/styles.css';
+import './app.less';
 
-// import Aside from './Components/Aside';
+import Aside from './Components/Aside';
 import Content from './Components/Content';
 
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
-// injectTapEventPlugin();
+injectTapEventPlugin();
 
 const App = () => (
   <div className="box">
@@ -28,11 +28,11 @@ const App = () => (
         <div className="box">
           <div className="row">
 
-            {/*<div className="col-xs-3">
+            <div className="col-xs-3">
               <div className="box">
                 <Aside />
               </div>
-            </div>*/}
+            </div>
 
             <div className="col-xs-9">
               <div className="box">
